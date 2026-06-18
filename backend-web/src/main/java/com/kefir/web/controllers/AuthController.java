f843689bb161c7cffda4126920c4ce48b6179d03
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-06-18 10:56:39 -0300

    CORS configuration added

--- backend-web/src/main/java/com/kefir/web/controllers/AuthController.java
+++ backend-web/src/main/java/com/kefir/web/controllers/AuthController.java
@@ -6,13 +6,11 @@ import com.kefir.web.dtos.auth.LoginRequest;
 import com.kefir.web.dtos.auth.RefreshRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 @Tag(name = "User authorization services", description = "APIs for user authorization")
 public class AuthController {
   private final AuthService authService;
