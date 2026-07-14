781bbe212bf29f76070ea538127a0cfdbab01668
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-07-14 15:33:15 -0300

    Added string size validation to name fields in customerRequest

--- backend-web/src/main/java/com/kefir/web/dtos/customer/CustomerCreationRequest.java
+++ backend-web/src/main/java/com/kefir/web/dtos/customer/CustomerCreationRequest.java
@@ -5,14 +5,15 @@ import com.kefir.enums.DocumentType;
 import com.kefir.enums.PersonType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record CustomerCreationRequest(
-    @NotBlank(message = "Name1 is mandatory") String name1,
-    String name2,
-    String name3,
-    @NotBlank(message = "Lastname1 is mandatory") String lastname1,
-    String lastname2,
-    String lastname3,
+    @NotBlank(message = "Name1 is mandatory") @Size(max = 20) String name1,
+    @Size(max = 20) String name2,
+    @Size(max = 20) String name3,
+    @Size(max = 20) @NotBlank(message = "Lastname1 is mandatory") String lastname1,
+    @Size(max = 20) String lastname2,
+    @Size(max = 20) String lastname3,
     @NotNull(message = "Person type is mandatory") PersonType personType,
     @NotNull(message = "Document type is mandatory") DocumentType documentType,
     @NotNull(message = "Document number is mandatory") String documentNumber,
