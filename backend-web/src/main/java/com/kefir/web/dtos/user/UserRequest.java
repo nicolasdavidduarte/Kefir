e0998c24183de372e47daf61142b61fca8f152ff
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-07-20 09:47:29 -0300

    Regex for password control added to User Request

--- backend-web/src/main/java/com/kefir/web/dtos/user/UserRequest.java
+++ backend-web/src/main/java/com/kefir/web/dtos/user/UserRequest.java
@@ -4,20 +4,17 @@ import com.kefir.enums.UserRoles;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import java.util.List;
 
 public record UserRequest(
-        @NotBlank
-        @Size(max = 10)
-        String username,
-        @NotBlank
+    @NotBlank @Size(max = 10) String username,
+    @NotBlank
         @Pattern(
-                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._#\\-])[A-Za-z\\d@$!%*?&._#\\-]{8,255}$",
-                message = "Password must be 8-64 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character."
-        )
+            regexp =
+                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._#\\-])[A-Za-z\\d@$!%*?&._#\\-]{8,255}$",
+            message =
+                "Password must be 8-64 characters long and contain at least one uppercase letter,"
+                    + " one lowercase letter, one number and one special character.")
         String password,
-        @NotBlank
-        @Size(max = 50)
-        String fullname,
+    @NotBlank @Size(max = 50) String fullname,
     List<UserRoles> roles) {}
