285224e7ef22cdcb4c652a915b2551cb39c50c43
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-08-24 14:48:29 -0300

    RegEx for password fixed at UserRequest

--- backend-web/src/main/java/com/kefir/web/dtos/user/UserRequest.java
+++ backend-web/src/main/java/com/kefir/web/dtos/user/UserRequest.java
@@ -10,11 +10,10 @@ public record UserRequest(
     @NotBlank @Size(max = 10) String username,
     @NotBlank
         @Pattern(
-            regexp =
-                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&._#\\-])[A-Za-z\\d@$!%*?&._#\\-]{8,255}$",
+            regexp = "^(?=.*\\p{Ll})(?=.*\\p{Lu})(?=.*\\p{N})(?=.*[^\\p{L}\\p{N}\\s])\\S{8,255}$",
             message =
-                "Password must be 8-64 characters long and contain at least one uppercase letter,"
-                    + " one lowercase letter, one number and one special character.")
+                "Password must be at least 8 characters long and contain at least one uppercase"
+                    + " letter, one lowercase letter, one number and one special character.")
         String password,
     @NotBlank @Size(max = 50) String fullname,
     List<UserRoles> roles) {}
