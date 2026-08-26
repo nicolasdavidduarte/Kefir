7f21b2645cd33470f3e0a5272c2e9ad0d61626b0
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-08-26 15:01:15 -0300

    Added EntityGraph to Customer Type repo methods

--- domain/src/main/java/com/kefir/repositories/CustomerTypeRepository.java
+++ domain/src/main/java/com/kefir/repositories/CustomerTypeRepository.java
@@ -3,6 +3,8 @@ package com.kefir.repositories;
 import com.kefir.entities.CustomerType;
 import java.util.List;
 import java.util.Optional;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +12,10 @@ import org.springframework.stereotype.Repository;
 public interface CustomerTypeRepository extends JpaRepository<CustomerType, Integer> {
   Optional<CustomerType> findByNameIgnoreCase(String name);
 
+  @Override
+  @EntityGraph(attributePaths = {"createdBy", "updatedBy"})
+  @NonNull Optional<CustomerType> findById(@NonNull Integer Id);
+
+  @EntityGraph(attributePaths = {"createdBy", "updatedBy"})
   List<CustomerType> findAllByOrderByIdAsc();
 }
