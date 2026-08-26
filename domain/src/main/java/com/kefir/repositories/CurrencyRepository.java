e84b0963af6d5368794277f1c1e2c1589fd8192a
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-08-26 14:55:06 -0300

    Added EntityGraph to currency repository methods

--- domain/src/main/java/com/kefir/repositories/CurrencyRepository.java
+++ domain/src/main/java/com/kefir/repositories/CurrencyRepository.java
@@ -3,13 +3,21 @@ package com.kefir.repositories;
 import com.kefir.entities.Currency;
 import java.util.List;
 import java.util.Optional;
+import org.jspecify.annotations.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
 
+  @EntityGraph(attributePaths = {"createdBy", "updatedBy"})
   Optional<Currency> findByIsoCode(String isoCode);
 
+  @Override
+  @EntityGraph(attributePaths = {"createdBy", "updatedBy"})
+  @NonNull Optional<Currency> findById(@NonNull Integer id);
+
+  @EntityGraph(attributePaths = {"createdBy", "updatedBy"})
   List<Currency> findAllByOrderByIdAsc();
 }
