baa15fefcead149ab0ac825410f4cf00b8eb0ea9
nicolasdavidduarte <nicolasdavidduarte@gmail.com>
2026-08-26 15:33:15 -0300

    UserResponse updated

--- backend-web/src/main/java/com/kefir/web/dtos/user/UserResponse.java
+++ backend-web/src/main/java/com/kefir/web/dtos/user/UserResponse.java
@@ -6,12 +6,18 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 public record UserResponse(
-    Integer id, String username, Boolean enabled, OffsetDateTime createdAt, List<String> roles) {
+    Integer id,
+    String username,
+    Boolean enabled,
+    String createdBy,
+    OffsetDateTime createdAt,
+    List<String> roles) {
   public static UserResponse fromEntity(User user) {
     return new UserResponse(
         user.getId(),
         user.getUsername(),
         user.isEnabled(),
+        user.getCreatedBy().getUsername(),
         user.getCreatedAt(),
         user.getRoles().stream().map(Role::getName).toList());
   }
